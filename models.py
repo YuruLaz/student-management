ce89c70702030e47125f073b16ad155c93de814b
lazare sharashenidze <sharashenidze2003@gmail.com>
2026-07-31 16:23:08 +0400

    addition to models

--- models.py
+++ models.py
@@ -15,13 +15,19 @@ class Person(ABC):
         print(f"name: {self._name}")
         print(f"ID_number: {self._roll_number}")
 
+    @property
+    def roll_number(self):
+        return self._roll_number
 
+    @property
+    def name(self):
+        return self._name
 
 class Student(Person):
 
     @staticmethod
     def validate_grade(grade):
-        if not isinstance(grade, int) or not 0 < grade < 100:
+        if not isinstance(grade, int) or not 0 <= grade <= 100:
             raise InvalidGradeError(grade)
 
         return grade
@@ -44,6 +50,7 @@ class Student(Person):
     def grade(self, new_grade):
         self._grade = self.validate_grade(new_grade)
 
+    
 
     def __str__(self):
         return f"{self._name} (#{self._roll_number}) - grade: {self._grade}"
@@ -52,8 +59,11 @@ class Student(Person):
         return f"Student(name={self._name!r}, number={self._roll_number!r}, grade={self._grade!r})"
 
     def __lt__(self, other):
-        return self._grade < other._grade
+        return isinstance(other, Student) and self._grade < other._grade
 
     def __eq__(self, other):
         return isinstance(other, Student) and self._roll_number == other._roll_number
+
+    def __hash__(self):
+        return hash(self._roll_number)
 
